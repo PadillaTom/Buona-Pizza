c87b8ddf7d45349c6ccc4b109be4c6b711550af6
Tomas Padilla <padillatomasagustin@gmail.com>
2020-10-07 20:08:05 +0200

    trying fix to firebase imgs

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ProductCard = ({ title, description, imageUrl, price, page }) => {
+const ProductCard = ({ key, title, description, imageUrl, price, page }) => {
   // Main:
   return (
     <article className='single-product'>
