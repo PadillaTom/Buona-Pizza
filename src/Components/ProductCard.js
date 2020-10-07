8a562fbb65415a0012179fdb47d24818a14f22b1
Tomas Padilla <padillatomasagustin@gmail.com>
2020-10-07 19:59:56 +0200

    firebase images error

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ProductCard = ({ id, title, description, imageUrl, price }) => {
+const ProductCard = ({ title, description, imageUrl, price, page }) => {
   // Main:
   return (
     <article className='single-product'>
-      <img src={imageUrl} alt={title} className='product-img' />
+      <img src={imageUrl} alt={page} className='product-img' />
       <div className='product-info'>
         <h2 className='product-title'>{title}</h2>
         <p className='product-price'>
           <small>$</small> {price}
         </p>
-        <Link to={`menu/${id}`} className='btn-primary btn-card'>
+        <Link to={`menu/${page}`} className='btn-primary btn-card'>
           Details
         </Link>
       </div>
