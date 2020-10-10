cac133bf76eb78342775e77f786489cdee02e96a
Tomas Padilla <padillatomasagustin@gmail.com>
2020-10-10 12:26:51 +0200

    Slider Done, agregar algo mas y listo

--- src/Slider/SliderMain.js
+++ src/Slider/SliderMain.js
@@ -7,21 +7,48 @@ function SliderMain() {
  const [image, setImage] = useState(sliderImgs);
  const [index, setIndex] = useState(0);
 
+ // Array Last item or -1 Item:
+ useEffect(()=>{
+     const lastIndex = image.length -1;
+if (index < 0){
+    setIndex(lastIndex)
+}
+if (index > lastIndex){
+    setIndex(0)
+}
+ },[index,image])
+ 
+ // Auto Play:
+ useEffect(()=>{
+let sliderTimer = setInterval(()=>{
+setIndex(index +1)
+},3500);
+return ()=> clearInterval(sliderTimer)
+ },[index])
+
     // Main:
     return (
         <section className="section">
             <div className="gallery-container">
 {image.map((imageItem, imageIndex)=>{
     const {id, image} = imageItem;
-    // More Stuff
-    return <article key={id}>
-        <img src={image} alt="gallery"/>
-    </article>
+    // More Stuff:
+    let position ="nextSlide";
+    if (imageIndex === index){
+        position= "activeSlide"
+    }
+    if(imageIndex === index-1 || (index === 0 && imageIndex === image.length-1 )){
+        position= "lastSlide"
+    }
+    return <article key={id} className={`slider-article ${position}`} >
+        <img   src={image} alt="gallery" className="gallery-img"/>
+        </article>
+   
 })} 
-<button className="prev-slide">
+<button className="slide-btn prev-slide" onClick={()=>setIndex(index-1)}>
     <GrPrevious></GrPrevious>
 </button>
-<button className="next-slide">
+<button className="slide-btn next-slide" onClick={()=>setIndex(index+1)}>
     <GrNext></GrNext>
 </button>
 </div>
